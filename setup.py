96a4d95b1f5470329a55113440f1a4849cf3bbbf
Byeonghoon Yoo <bh322yoo@gmail.com>
2019-01-19 22:15:11 +0900

    bump up version to 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
         name='linux_aio',
-        version='0.2.0',
+        version='0.2.1',
         author='Byeonghoon Yoo',
         author_email='bh322yoo@gmail.com',
         description='Linux aio ABI wrapper',
@@ -21,6 +21,7 @@ setup(
             'Operating System :: POSIX :: Linux',
             'Programming Language :: C',
             'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
@@ -35,7 +36,7 @@ setup(
         ],
         license='LGPLv3+',
         keywords='Linux AIO ABI API wrapper',
-        python_requires='>=3.5',
+        python_requires='>=3.4',
         provides=['linux_aio'],
         platforms='Linux',
         setup_requires=['cffi>=1.0.0'],
