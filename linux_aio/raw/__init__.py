3f0473ee4e9eb9f622c08540ae5255c8c88d255d
Byeonghoon Yoo <bh322yoo@gmail.com>
2019-01-22 01:55:26 +0900

    implement raw.iocb.IOVec with ctypes
    (cherry picked from commit e2ef47c21e13e8a3562072800a23eb5d508b8c21)

--- linux_aio/raw/__init__.py
+++ linux_aio/raw/__init__.py
@@ -1,4 +1,4 @@
 # coding: UTF-8
 
 from .aio import IOEvent, Timespec, aio_context_t, io_cancel, io_destroy, io_getevents, io_setup, io_submit, iocb_p
-from .iocb import IOCB, IOCBCMD, IOCBFlag, IOCBPriorityClass, IOCBRWFlag, IOPRIO_CLASS_SHIFT, gen_io_priority
+from .iocb import IOCB, IOCBCMD, IOCBFlag, IOCBPriorityClass, IOCBRWFlag, IOPRIO_CLASS_SHIFT, IOVec, gen_io_priority
