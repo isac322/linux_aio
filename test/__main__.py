6eb02e26fdf921e5b24105ad4a07839d3fe81bc3
Byeonghoon Yoo <bh322yoo@gmail.com>
2019-01-22 03:26:57 +0900

    add test_non_rw.py for testing Non-RW AIO operations
    (cherry picked from commit 06ad8b304d2d2aaca31c1110ff488be8dca83152)

--- test/__main__.py
+++ test/__main__.py
@@ -5,6 +5,8 @@ import unittest
 # noinspection PyUnresolvedReferences
 from .test_context import TestContext
 # noinspection PyUnresolvedReferences
+from .test_non_rw import TestNonRW
+# noinspection PyUnresolvedReferences
 from .test_rw import TestRW
 
 if __name__ == '__main__':
